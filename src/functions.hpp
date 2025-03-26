4a759dca6d73f716fd24e9e239382c486354731d
Ryl <rylmelange@gmail.com>
2025-03-26 01:15:02 +0000

    moved setup default gate into functions.cpp

---
+++ src/functions.hpp
@@ -0,0 +1,5 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void setup_default_MUL_circuit();
+#endif 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "gates.hpp"
+#include "functions.hpp"
 using namespace std;
 
 // define stuff (code from tsoding)
@@ -26,31 +27,7 @@ int main(int argc, char **argv) {
     printf("AAAAAAAA what on earth does '%s' mean???\n", command_name);
     return -1;
   } else {
-    circuit.insert({0, make_unique<InputGate>("input", Vector2{0, 0},
-                                              vector<Packet>{-1, -1},
-                                              vector<Port>{{1, 0}, {2, 0}})});
-    circuit.insert(
-        {1, make_unique<SplitGate>("split1", Vector2{100, 0}, vector<Packet>{0},
-                                   vector<Port>{{3, 0}, {4, 0}})});
-    circuit.insert(
-        {2, make_unique<SplitGate>("split2", Vector2{0, 100}, vector<Packet>{0},
-                                   vector<Port>{{3, 1}, {4, 1}})});
-    circuit.insert(
-        {3, make_unique<MaxGate>("max1", Vector2{200, 0}, vector<Packet>{0, 0},
-                                 vector<Port>{{5, 0}})});
-    circuit.insert(
-        {4, make_unique<MinGate>("min1", Vector2{0, 200}, vector<Packet>{0, 0},
-                                 vector<Port>{{6, 1}})});
-    circuit.insert(
-        {5, make_unique<NegGate>("neg", Vector2{200, 200}, vector<Packet>{0},
-                                 vector<Port>{{6, 0}})});
-    circuit.insert(
-        {6, make_unique<MaxGate>("max2", Vector2{300, 300},
-                                 vector<Packet>{0, 0}, vector<Port>{{-1, 0}})});
-    circuit.insert(
-        {-1, make_unique<OutputGate>("output", Vector2{700, 700},
-                                     vector<Packet>{0}, vector<Port>{})});
-    components_to_update.insert(0);
+    setup_default_MUL_circuit();
   }
 
   // Initialization
