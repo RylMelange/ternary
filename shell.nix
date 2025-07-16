96e0058a7c96d7acc1db52279ccefd66b64f88b5
Friday <faeriethe@gmail.com>
2025-07-16 09:08:21 +0000

    cleanup

--- shell.nix
+++ shell.nix
@@ -1,3 +1,4 @@
+# https://gist.github.com/gabrieldlima/0bf82c01ce54976ffc6f900639c36f51
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
@@ -5,24 +6,24 @@ pkgs.mkShell {
     gcc
     gnumake
     cmake
-    # raylib
+    raylib
     
-    pkgs.libGL
-
-    # X11 dependencies
-    pkgs.xorg.libX11
-    pkgs.xorg.libX11.dev
-    pkgs.xorg.libXcursor
-    pkgs.xorg.libXi
-    pkgs.xorg.libXinerama
-    pkgs.xorg.libXrandr
+    # pkgs.libGL
+    #
+    # # X11 dependencies
+    # pkgs.xorg.libX11
+    # pkgs.xorg.libX11.dev
+    # pkgs.xorg.libXcursor
+    # pkgs.xorg.libXi
+    # pkgs.xorg.libXinerama
+    # pkgs.xorg.libXrandr
 
     # Uncomment the line below if you want to build Raylib with web support
     # pkgs.emscripten
   ];
 
-  shellHook = ''
-    nvim ./src/main.cpp && exit
-  '';
+  # shellHook = ''
+  #   nvim ./src/main.cpp && exit
+  # '';
   # do nix-shell -p bear && bear -- make to obtain the ./compile_commands.json
 }
